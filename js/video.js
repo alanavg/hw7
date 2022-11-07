2ef13199d5456bd23f85f683574d1668bb6df605
Alana Vang <112514034+alanavg@users.noreply.github.com>
2022-11-07 14:23:28 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -21,8 +21,50 @@ document.querySelector("#pause").addEventListener("click", function() {
 	video.pause()
 });
 
+
 document.querySelector("#slower").addEventListener("click", function() {
 	video.playbackRate = video.playbackRate * 0.90;
+	console.log("Speed down")
 	console.log("Video speed is " + video.playbackRate)
 });
 
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate / 0.90;
+	console.log("Speed up")
+	console.log("Video speed is " + video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime = video.currentTime + 10;
+
+	console.log("Skip ahead")
+	if (video.currentTime == 67.403333) {
+		video.currentTime = video.currentTime * 0;
+		console.log("Video is at " + video.currentTime)
+	}
+	else {
+		console.log("Video is at " + video.currentTime)
+	}
+	
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (document.querySelector("#mute").innerHTML == "Mute") {
+		console.log("Mute")
+		video.volume = 0;
+		document.querySelector("#mute").innerHTML= "Unmute"
+		document.querySelector("#volume").innerHTML= video.volume * 100 + "%"
+	}
+	else {
+		console.log("Unmute")
+		document.querySelector("#mute").innerHTML == "Unmute"
+		video.volume = 1.0;
+		document.querySelector("#mute").innerHTML= "Mute"
+		document.querySelector("#volume").innerHTML= video.volume * 100 + "%"
+	}
+});
+
+document.querySelector("slider").addEventListener("click", function() {
+	document.querySelector("#volume").innerHTML= "%"
+	console.log("Volume is " + video.volume*100)
+});
